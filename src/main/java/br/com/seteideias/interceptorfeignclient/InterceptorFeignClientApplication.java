e2f5315f73a02dabc4cae17c5f423f3cc2cfb4fa
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2022-07-11 23:00:44 -0300

    ajustes no feign client

--- src/main/java/br/com/seteideias/interceptorfeignclient/InterceptorFeignClientApplication.java
+++ src/main/java/br/com/seteideias/interceptorfeignclient/InterceptorFeignClientApplication.java
@@ -2,8 +2,10 @@ package br.com.seteideias.interceptorfeignclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClient
+
+@EnableFeignClients
 @SpringBootApplication
 public class InterceptorFeignClientApplication {
 
--- src/main/java/br/com/seteideias/interceptorfeignclient/client/OrderSenderUello.java
+++ src/main/java/br/com/seteideias/interceptorfeignclient/client/OrderSenderUello.java
@@ -2,12 +2,12 @@ package br.com.seteideias.interceptorfeignclient.client;
 
 import br.com.seteideias.interceptorfeignclient.entity.uello.OrderUello;
 import br.com.seteideias.interceptorfeignclient.entity.uello.OrderUelloResponse;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@Fei
-//@FeignClient(name = "uello", url = "${client.uello.task.url}")
+@FeignClient(name = "uello", url = "${client.uello.task.url}")
 public interface OrderSenderUello {
 
     @PostMapping(value = "/orders", consumes = "application/json",produces = "application/json")
